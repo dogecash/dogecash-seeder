c1362aa5a8672616724d889f740008610702918b
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-06-13 09:51:27 -0500

    Update ports

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 16470 : 6470;
+    return testnet ? 56470 : 56470;
 }
 
 //
